39a0d5de1612d4b08c8bac86c8774198b49e3ac9
T-107 <T-107@PC257>
2015-05-25 18:13:29 -0500

    clase 21 Mayo

--- SergioJoelJava2/src/abstraccion/InterfazImc.java
+++ SergioJoelJava2/src/abstraccion/InterfazImc.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstraccion;
 
-/**
- *
- * @author T-107
- */
+
 public class InterfazImc {
     public static void main (String []oso){
   
