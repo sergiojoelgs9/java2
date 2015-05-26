8b53db5c2333d8b46287f720023c2dab8ad1f728
T-107 <T-107@PC257>
2015-05-25 19:35:34 -0500

    clase 25 Mayo

---
+++ SergioJoelJava2/src/Encapsulacion/ProbarEncapsulamiento.java
@@ -0,0 +1,16 @@
+
+package Encapsulacion;
+
+
+public class ProbarEncapsulamiento {
+
+    
+    public static void main(String[] args) {
+        
+        Usuario u1=new  Usuario();
+      
+        
+
+    }
+    
+}
---
+++ SergioJoelJava2/src/Encapsulacion/Usuario.java
@@ -0,0 +1,33 @@
+
+package Encapsulacion;
+
+
+
+
+public class Usuario {
+    
+    
+   private String nombre;
+   private int edad;
+
+   
+   //public es un motador en la encapsulacion
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
