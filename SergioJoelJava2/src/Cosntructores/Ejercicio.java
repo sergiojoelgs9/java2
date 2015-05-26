fe50aadb1c86b5500881e5adbcfeeacae5a9c594
T-107 <T-107@PC257>
2015-05-25 19:05:52 -0500

    clase 25 Mayo

---
+++ SergioJoelJava2/src/Cosntructores/Ejercicio.java
@@ -0,0 +1,17 @@
+
+package Cosntructores;
+
+
+public class Ejercicio {
+    
+    int x;
+    float y;
+    
+    //no debe estar duplicado se repite la regla inetro flotante
+    
+    //public Test1(int x,float y){}
+    //public Test1(float x,int y){}
+    //public Test1(int y,float x){} ......este esta mal se duplica
+    
+    
+}
