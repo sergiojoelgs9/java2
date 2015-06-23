c8b62e1ea82e77988c81d6f3c12667a770bf7fbb
T-107 <T-107@PC258>
2015-06-22 19:03:11 -0500

    ejercicio tipo examen 22/06/15

---
+++ SergioJoelJava2/src/EjercicioTipoExamen/Direccion.java
@@ -0,0 +1,60 @@
+
+package EjercicioTipoExamen;
+
+public class Direccion {
+    
+    private String calle;
+    private String colonia;
+    private String municipio;
+    private int cp;
+    private int numero;
+
+    public Direccion() {
+    }
+
+    public Direccion(String municipio, int cp) {
+        this.municipio = municipio;
+        this.cp = cp;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+}
---
+++ SergioJoelJava2/src/EjercicioTipoExamen/Usuario.java
@@ -0,0 +1,56 @@
+
+package EjercicioTipoExamen;
+
+
+public class Usuario extends Direccion{
+    
+    private String nombre;
+    private int edad;
+    private String email;
+    private Direccion direccion;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, int edad, String email, Direccion direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+        this.direccion = direccion;
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+   
+}
